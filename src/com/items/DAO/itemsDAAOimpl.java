a3757351452f4dfa81a62811a1a9d4bba5e5e328
User <User@LAPTOP-8I2MFFDV>
2021-02-25 21:55:19 +0530

    implement the list items

--- src/com/items/DAO/itemsDAAOimpl.java
+++ src/com/items/DAO/itemsDAAOimpl.java
@@ -2,14 +2,15 @@ package com.items.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import com.items.util.*;
 
 import com.items.util.DBConnect;
 
-public class itemsDAAOimpl implements ItemsDAO {
+public class itemsDAAOimpl  {
 
-	@Override
 	public String insertItem(String code, String name, String price, String desc) {
 		
 		String output ="";
@@ -53,7 +54,66 @@ public class itemsDAAOimpl implements ItemsDAO {
 		return output;
 	}
 	
+	public String readItems()
+	{
+		
+		String output = "";
+		
+		try
+		{
+		Connection con = DBConnect.DBconnection();
+		
+		if (con == null)
+		{
+			return "Error while connecting to the database for reading.";
+		}
+		// Prepare the html table to be displayed
+		output = "<table border='1'><tr><th>Item Code</th>"
+		+"<th>Item Name</th><th>Item Price</th>"
+		+ "<th>Item Description</th>"
+		+ "<th>Update</th><th>Remove</th></tr>";
+		
+		String query = "select * from items";
+		
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		// iterate through the rows in the result set
+		while (rs.next())
+		{
+			String itemID = Integer.toString(rs.getInt("itemID"));
+			String itemCode = rs.getString("itemCode");
+			String itemName = rs.getString("itemName");
+			String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+			String itemDesc = rs.getString("itemDesc");
+			// Add a row into the html table
+			output += "<tr><td>" + itemCode + "</td>";
+			output += "<td>" + itemName + "</td>";
+			output += "<td>" + itemPrice + "</td>";
+			
+			output += "<td>" + itemDesc + "</td>";
+			// buttons
+			output += "<td><input name='btnUpdate' "
+			+ " type='button' value='Update'></td>"
+			+ "<td><form method='post' action='items.jsp'>"
+			+ "<input name='btnRemove' "
+			+ " type='submit' value='Remove'>"
+			+ "<input name='itemID' type='hidden' "
+			+ " value='" + itemID + "'>" + "</form></td></tr>";
+		}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		}
+			catch (Exception e)
+		{
+			output = "Error while reading the items.";
+			System.err.println(e.getMessage());
+			}
+			return output;	
+	}
 	
+	
+	3
 	
 
 }
