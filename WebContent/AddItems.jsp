ec39b352b512cf3cf8c252e529aa21a5cf72d56c
User <User@LAPTOP-8I2MFFDV>
2021-02-25 22:45:25 +0530

    Completed the implementation of the inserting and reading function

--- WebContent/AddItems.jsp
+++ WebContent/AddItems.jsp
@@ -26,16 +26,19 @@ if (request.getParameter("ItemCode") != null)
 
 	<form method="post" action="AddItems.jsp">
 	
-		Item code: <input name="itemCode" type="text"><br>
-		Item name: <input name="itemName" type="text"><br>
-		Item price: <input name="itemPrice" type="text"><br>
-		Item description: <input name="itemDesc" type="text"><br>
+		Item code: <input name="ItemCode" type="text"><br>
+		Item name: <input name="ItemName" type="text"><br>
+		Item price: <input name="ItemPrice" type="text"><br>
+		Item description: <input name="ItemDesc" type="text"><br>
 		
 		<input name="btnSubmit" type="submit" value="Save">
 	</form>
 
 <%
 		out.print(session.getAttribute("statusMsg"));
+		itemsDAAOimpl itemObj = new itemsDAAOimpl();
+        out.print(itemObj.readItems());
+
 %>
 </body>
 </html>
--- src/com/items/DAO/itemsDAAOimpl.java
+++ src/com/items/DAO/itemsDAAOimpl.java
@@ -26,7 +26,7 @@ public class itemsDAAOimpl  {
 			 }else {
 				 
 				//create a prepared statement
-				  String query = "insert into items (itemID, itemCode, ItemPrice, ItemDesc)"
+				  String query = "insert into item (ItemID,ItemCode,ItemName, ItemPrice, ItemDesc)"
 				  		+ " values(?,?,?,?,?)";
 				  
 				  PreparedStatement preparedstmt = con.prepareStatement(query);
@@ -61,59 +61,59 @@ public class itemsDAAOimpl  {
 		
 		try
 		{
-		Connection con = DBConnect.DBconnection();
+			Connection con = DBConnect.DBconnection();
 		
 		if (con == null)
 		{
 			return "Error while connecting to the database for reading.";
 		}
-		// Prepare the html table to be displayed
-		output = "<table border='1'><tr><th>Item Code</th>"
-		+"<th>Item Name</th><th>Item Price</th>"
-		+ "<th>Item Description</th>"
-		+ "<th>Update</th><th>Remove</th></tr>";
-		
-		String query = "select * from items";
-		
-		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery(query);
-		// iterate through the rows in the result set
-		while (rs.next())
-		{
-			String itemID = Integer.toString(rs.getInt("itemID"));
-			String itemCode = rs.getString("itemCode");
-			String itemName = rs.getString("itemName");
-			String itemPrice = Double.toString(rs.getDouble("itemPrice"));
-			String itemDesc = rs.getString("itemDesc");
-			// Add a row into the html table
-			output += "<tr><td>" + itemCode + "</td>";
-			output += "<td>" + itemName + "</td>";
-			output += "<td>" + itemPrice + "</td>";
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Item Code</th>"
+			+"<th>Item Name</th><th>Item Price</th>"
+			+ "<th>Item Description</th>"
+			+ "<th>Update</th><th>Remove</th></tr>";
 			
-			output += "<td>" + itemDesc + "</td>";
-			// buttons
-			output += "<td><input name='btnUpdate' "
-			+ " type='button' value='Update'></td>"
-			+ "<td><form method='post' action='items.jsp'>"
-			+ "<input name='btnRemove' "
-			+ " type='submit' value='Remove'>"
-			+ "<input name='itemID' type='hidden' "
-			+ " value='" + itemID + "'>" + "</form></td></tr>";
+			String query = "select * from item";
+			
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+		
+			// iterate through the rows in the result set
+			while (rs.next())
+				{
+					String itemID = Integer.toString(rs.getInt("ItemID"));
+					String itemCode = rs.getString("ItemCode");
+					String itemName = rs.getString("ItemName");
+					String itemPrice = Double.toString(rs.getDouble("ItemPrice"));
+					String itemDesc = rs.getString("ItemDesc");
+					
+					// Add a row into the html table
+					output += "<tr><td>" + itemCode + "</td>";
+					output += "<td>" + itemName + "</td>";
+					output += "<td>" + itemPrice + "</td>";
+					
+					output += "<td>" + itemDesc + "</td>";
+					// buttons
+					output += "<td><input name='btnUpdate' " + " type='button' value='Update'></td>"
+					+ "<td><form method='post' action='AddItems.jsp'>"
+					+ "<input name='btnRemove' "+ " type='submit' value='Remove'>"
+					+ "<input name='ItemID' type='hidden' "
+					+ " value='" + itemID + "'>" + "</form></td></tr>";
+				}
+					con.close();
+					// Complete the html table
+					output += "</table>";
+				}
+					catch (Exception e)
+				{
+					output = "Error while reading the items.";
+					System.err.println(e.getMessage());
+					}
+					return output;	
 		}
-			con.close();
-			// Complete the html table
-			output += "</table>";
-		}
-			catch (Exception e)
-		{
-			output = "Error while reading the items.";
-			System.err.println(e.getMessage());
-			}
-			return output;	
-	}
 	
 	
-	3
+	
 	
 
 }
